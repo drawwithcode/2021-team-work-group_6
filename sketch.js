18fff7210c9f276746be05ac85745e452c26104c
saragussoni <gussoni.sara@gmail.com>
2022-01-22 09:54:53 +0100

    scrittescorrimento
    scrittescorrimento sto provando a fare la seconda

--- sketch.js
+++ sketch.js
@@ -1,3 +1,31 @@
+//animationscrolltexts//
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+let outer = document.querySelector("#outer");
+let content = outer.querySelector('#content');
+
+repeatContent(content, outer.offsetWidth);
+
+let el = outer.querySelector('#loop');
+el.innerHTML = el.innerHTML + el.innerHTML;
+
+function repeatContent(el, till) {
+    let html = el.innerHTML;
+    let counter = 0; // prevents infinite loop
+    
+    while (el.offsetWidth < till && counter < 100) {
+        el.innerHTML += html;
+        counter += 1;
+    }
+}
+
 // organic is used to store the list of instances of Organic objects that we will create
 let blobs = [];
 // Daniel Shiffman
