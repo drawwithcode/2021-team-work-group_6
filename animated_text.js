43eac819db22050d6b5274017461416cc844ac0e
Brioche23 <alessio.brioche@gmail.com>
2022-01-23 17:12:44 +0100

    Added text animation screen 1

--- animated_text.js
+++ animated_text.js
@@ -1,15 +1,15 @@
 // *Animation Scroll Texts
 const frase = "PLEASE STAND IN FRONT OF YOUR BLOB ●&nbsp;";
 let content = document.getElementsByClassName("loop");
-content.forEach((c) => {
-  repeatContent(c, c.offsetWidth);
-  function repeatContent(el) {
-    // let html = el.innerHTML;
-    let counter = 0; // prevents infinite loop
-    const max_testi = 10;
-    while (counter < max_testi) {
-      el.innerHTML += frase;
-      counter += 1;
+if (screen_1)
+  content.forEach((c) => {
+    repeatContent(c, c.offsetWidth);
+    function repeatContent(el) {
+      let counter = 0; // prevents infinite loop
+      const max_testi = 10;
+      while (counter < max_testi) {
+        el.innerHTML += frase;
+        counter += 1;
+      }
     }
-  }
-});
+  });
